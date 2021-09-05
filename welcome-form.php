0a44c2c397a57a6ad5dff9caa6cba1910b4506bb
ReportDataCode <andrewcustomerhelper@gmail.com>
2021-09-05 17:45:46 +0800

    Added Example PHP Forms

---
+++ welcome-form.php
@@ -0,0 +1,12 @@
+<html>
+
+<body>
+
+    <form action="welcome.php" method="GET">
+        name: <input type="text" name="firstName" id="#">
+        Age: ><input type="text" name="age" id="#">
+        <input type="submit">
+    </form>
+</body>
+
+</html>
